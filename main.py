d7152b27a60e3e4d6bc855c107aac01241c4fd2c
jsievert <jsievert@iu.edu>
2019-08-28 17:10:40 -0400

    Updates LICENSE and main.py

--- main.py
+++ main.py
@@ -5,4 +5,12 @@ import utils
 utils.check_version((3,7))
 utils.clear()
 
-print('Hello, Class!')
+print('Hello, my name is Jacob Sievert.')
+username = input("What is you name?\n")
+print('Hello, {0}!'.format(username))
+favgame = input("What is your favorite game?\n")
+print('My favorite game is Kingdom Hearts 2. I think they aced the balance of variety of gameplay mechanics without overloading the player.')
+print('I do not really have any concerns about this class at the moment.')
+print('I am very excited to run my next D&D campaign. I have homebrewed an entire new class that I call the Reality Shifter.')
+print('My stackoverflow.com use number is 11991138.')
+print('Here is the url for my Github profile. https://github.com/jsievert73')
